60de45667ddf6872e93e397f220823f32b1c8e8f
henrybugbean <henrybugbean@icloud.com>
2026-06-14 13:10:07 +0100

    made a max amount of characters, make some global versiable and made personalislied shorter if theres a longer name

--- password_generator.py
+++ password_generator.py
@@ -1,11 +1,14 @@
 import random
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+digits = "0123456789"
+symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?/"
+max = 20
 
 def random_password(length):
-    
+    if length > max:
+        print("Password length exceeds maximum limit of 20 characters.")
+        return None
     password = ""
-    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    digits = "0123456789"
-    symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?/"
     while len(password) != length:
         password = random.choices(letters, k=random.randint(1, 4)) + random.choices(digits, k=random.randint(1, 4)) + random.choices(symbols, k=random.randint(1, 4))
         password_list = list(password)
@@ -24,9 +27,16 @@ password = random_password(9)
 print(password)
 
 def personalized_password(length, name):
-    password = random_password(length)
-    personalized = list(password[length//2:] + name + password[:length//2])
-    return "".join(personalized)
+    if length > max:
+        print("Password length exceeds maximum limit of 20 characters.")
+        return None
+    if len(name) >= length:
+        personalised = name + random.choices(letters + digits + symbols, k=random.randint(4, 6))
+        
+    else:
+        password = random_password(length)
+        personalised = list(password[length//2:] + name + password[:length//2])
+    return "".join(personalised)
     
 
 print(personalized_password(9, input("Enter your name: ")))
